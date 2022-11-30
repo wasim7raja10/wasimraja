a229bc8018ee37f9c0f3d610a7fb3ec98e43a5b0
Wasim Raja <wasim7raja10@gmail.com>
2022-11-30 23:37:19 +0530

    setup changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Jsblog1 } from './ui-components'
 function App() {
   return (
     <div className="App">
+      hello
       <Jsblog1 />
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
 
-import awsconfig from './aws-exports.js';
+import awsconfig from './aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
 import { studioTheme } from "./ui-components";
